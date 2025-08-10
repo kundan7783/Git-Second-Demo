c57e3a0062b411b179fad017650b0d0c330e3644
kundan7783 <kundan065335@gmail.com>
2025-08-10 20:40:56 +0530

    call Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:git_second_demo/screens/splash_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
    return MaterialApp(
      debugShowCheckedModeBanner: false,
-     home: ,
+     home: SplashScreen(),
    );
   }
 }
