a68a48ab75dd6da186e534f6888d592719fab40a
kundan7783 <kundan065335@gmail.com>
2025-08-11 21:41:50 +0530

    add a new future

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,6 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("Home Screen"),
         backgroundColor: Colors.blue,
       ),
+      body: Center(child: CircleAvatar(),),
     );
   }
 }
